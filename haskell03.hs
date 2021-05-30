159a0928cc2dfc836fbf9dbc826caa2df9483a0b
luizfelipecs <>
2021-05-30 19:12:10 +0000

    haskell 03 - 13/15

--- haskell03.hs
+++ haskell03.hs
@@ -1,1 +1,44 @@
 add10toall :: [Int] -> [Int]
+add10toall list = [x+10 | x <- list]
+
+multN :: Int -> [Int] -> [Int]
+multN n l = [x*n | x <- l]
+
+multN' :: Int -> [Int] -> [Int]
+multN' n l = map (*n) l
+
+applyExpr :: [Int] -> [Int]
+applyExpr list = [3*x+2 | x <- list]
+
+applyExpr' :: [Int] -> [Int]
+applyExpr' list = map (\x -> 3*x+2) list
+
+addSuffix :: String -> [String] -> [String]
+addSuffix str list = [x ++ str | x <- list]
+
+selectgt5 :: [Int] -> [Int]
+selectgt5 list = [x | x <- list, x > 5]
+
+sumOdds :: [Int] -> Int
+sumOdds list = sum [x | x <-list, odd x]
+
+sumOdds' :: [Int] -> Int
+sumOdds' list = sum (filter (\x -> odd x) list)
+
+selectExpr :: [Int] -> [Int]
+selectExpr list = [x | x <- list, x >= 20, x <= 50, even x]
+
+countShorts :: [String] -> Int
+countShorts list = length [x | x <- list, length x < 5]
+
+calcExpr :: [Float] -> [Float]
+calcExpr list = [x^2/2 | x <- list, x^2/2 > 10]
+
+trSpaces :: String -> String
+trSpaces str = [if x == ' ' then '-' else x | x <- str]
+
+--selectSnd :: [(Int,Int)] -> [Int]
+--selectSnd (a,b) = [ y | (_,y) <- [(a,b)]]
+
+--dotProd :: [Int] -> [Int] -> Int
+--dotProd l1 l2 = [zip l1 l2]
